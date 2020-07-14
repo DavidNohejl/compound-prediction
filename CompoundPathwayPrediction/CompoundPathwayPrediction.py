60b7b6422544fda13bc23a24c718c3eb823641ca
David Nohejl <davenohejl@gmail.com>
2020-07-14 15:01:06 +0200

    fix SVC, added MLP and AdaBoost

--- CompoundPathwayPrediction/CompoundPathwayPrediction.py
+++ CompoundPathwayPrediction/CompoundPathwayPrediction.py
@@ -7,10 +7,10 @@ from sklearn.tree import plot_tree
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 import numpy as np
-from skmultilearn.problem_transform import BinaryRelevance    
+from skmultilearn.problem_transform import BinaryRelevance ,   LabelPowerset
 from Evaluation import evaluate_classifier
 
-classifier = BinaryRelevance(classifier = DecisionTreeClassifier(random_state=0,max_depth=5))
+
 #classifier = DecisionTreeClassifier(random_state=0,max_depth=5)
 
 x_train,x_test,y_train,y_test = train_test_split(features,targets,test_size=0.33, random_state=42)
@@ -19,12 +19,17 @@ mlb = MultiLabelBinarizer()
 Y = mlb.fit_transform(y_train)
 Y_test = mlb.fit_transform(y_test)
 
+classifier = BinaryRelevance(classifier = DecisionTreeClassifier(random_state=0,max_depth=5))
 classifier = classifier.fit(x_train, Y)
 
 y_pred = classifier.predict(x_test)
 y_pred_decoded = mlb.inverse_transform(y_pred)
 
-evaluate_classifier(classifier,x_test,Y_test, y_pred)
+evaluate_classifier(classifier,x_test,Y_test, y_pred,"BinaryRelevance DecisionTreeClassifier(random_state=0,max_depth=5)")
+
+classifier = LabelPowerset(classifier = DecisionTreeClassifier(random_state=0,max_depth=5))
+classifier = classifier.fit(x_train, Y)
+evaluate_classifier(classifier,x_test,Y_test, y_pred,"LabelPowerset DecisionTreeClassifier(random_state=0,max_depth=10)")
 
 #plot_confusion_matrix(classifier,x_test,Y_test.argmax(axis=1))
 #plt.show()
@@ -47,16 +52,16 @@ score = 'f1_macro'
 x_train = np.matrix(np.reshape(np.hstack(x_train),(-1,7)))
 x_test = np.matrix(np.reshape(np.hstack(x_test),(-1,7)))
 
-clf = GridSearchCV(MLkNN(), parameters, scoring=score)
-clf.fit(x_train, Y)
+#clf = GridSearchCV(MLkNN(), parameters, scoring=score)
+#clf.fit(x_train, Y)
 
-print (clf.best_params_, clf.best_score_)
+#print (clf.best_params_, clf.best_score_)
 
 classifier = MLkNN(k=1,s=0.5)
 
 prediction = classifier.fit(x_train, Y).predict(x_test)
 
-evaluate_classifier(classifier,x_test,Y_test,prediction)
+evaluate_classifier(classifier,x_test,Y_test,prediction,"MLkNN(k=1,s=0.5)")
 
 
 from sklearn.ensemble import RandomForestClassifier
@@ -66,15 +71,78 @@ classifier = RandomForestClassifier(max_depth=5, n_estimators=10, max_features=1
 classifier.fit(x_train, Y)
 prediction = classifier.predict(x_test)
 
-evaluate_classifier(classifier,x_test,Y_test,prediction)
+evaluate_classifier(classifier,x_test,Y_test,prediction, "RandomForestClassifier")
 #seems to work, accuracy 89.79
 
 from sklearn.svm import SVC
 
-classifier = SVC(gamma=2, C=1)
+classifier = BinaryRelevance(classifier=SVC(gamma=2, C=1))
 
 print(x_train.shape, Y.shape)
-classifier.fit(x_train, Y) #ValueError: bad input shape (3726, 11)
+classifier.fit(x_train, Y)
 prediction = classifier.predict(x_test)
 
-evaluate_classifier(classifier,x_test,Y_test,prediction)
+evaluate_classifier(classifier,x_test,Y_test,prediction, "BinaryRelevance SVC")
+
+
+from sklearn.neural_network import MLPClassifier
+
+# default MLP, 100 hidden neurons, relu, adam, L2 penalty default=0.0001, const learning rate,default=0.001
+
+classifier = MLPClassifier(random_state=1, max_iter=300).fit(x_train, Y)
+prediction = classifier.predict(x_test)
+
+evaluate_classifier(classifier,x_test,Y_test,prediction,"MLPClassifier")
+
+classifier = MLPClassifier(random_state=1, max_iter=300,hidden_layer_sizes=(50,50)).fit(x_train, Y)
+prediction = classifier.predict(x_test)
+
+evaluate_classifier(classifier,x_test,Y_test,prediction,"MLPClassifier 50-50")
+
+classifier = MLPClassifier(random_state=1, max_iter=300,hidden_layer_sizes=(50,100,50)).fit(x_train, Y)
+prediction = classifier.predict(x_test)
+
+evaluate_classifier(classifier,x_test,Y_test,prediction,"MLPClassifier 50-100-50")
+
+classifier = MLPClassifier(random_state=1, max_iter=300,hidden_layer_sizes=(1000)).fit(x_train, Y)
+prediction = classifier.predict(x_test)
+
+evaluate_classifier(classifier,x_test,Y_test,prediction,"MLPClassifier 1000")
+
+
+from sklearn.ensemble import AdaBoostClassifier
+classifier = BinaryRelevance(classifier=AdaBoostClassifier(n_estimators=100, random_state=0)).fit(x_train, Y)
+prediction = classifier.predict(x_test)
+
+evaluate_classifier(classifier,x_test,Y_test,prediction,"BinaryRelevance AdaBoostClassifier")
+
+
+# from http://scikit.ml/modelselection.html#Estimating-hyper-parameter-k-for-embedded-classifiers
+# not helping much?  overall acc: 85.96%
+
+#from skmultilearn.problem_transform import ClassifierChain, LabelPowerset
+#from sklearn.model_selection import GridSearchCV
+#from sklearn.naive_bayes import GaussianNB
+#from sklearn.ensemble import RandomForestClassifier
+##pip install python-louvain
+#from skmultilearn.cluster import NetworkXLabelGraphClusterer
+#from skmultilearn.cluster import LabelCooccurrenceGraphBuilder
+#from skmultilearn.ensemble import LabelSpacePartitioningClassifier
+
+#from sklearn.svm import SVC
+
+#parameters = {
+#    'classifier': [LabelPowerset(), ClassifierChain()],
+#    'classifier__classifier': [RandomForestClassifier()],
+#    'classifier__classifier__n_estimators': [10, 20, 50],
+#    'clusterer' : [
+#        NetworkXLabelGraphClusterer(LabelCooccurrenceGraphBuilder(weighted=True, include_self_edges=False), 'louvain'),
+#        NetworkXLabelGraphClusterer(LabelCooccurrenceGraphBuilder(weighted=True, include_self_edges=False), 'lpa')
+#    ]
+#}
+
+#clf = GridSearchCV(LabelSpacePartitioningClassifier(), parameters, scoring = 'f1_macro')
+#clf.fit(x_train, Y)
+#prediction = clf.predict(x_test)
+#print (clf.best_params_, clf.best_score_)
+#evaluate_classifier(clf.best_estimator_.classifier,x_test,Y_test,prediction,"Grid Search")
--- CompoundPathwayPrediction/Evaluation.py
+++ CompoundPathwayPrediction/Evaluation.py
@@ -7,13 +7,14 @@ from sklearn.metrics import confusion_matrix,plot_confusion_matrix
 from sklearn.metrics import multilabel_confusion_matrix
 from PrepareData import metabolism_classes
 
-def evaluate_classifier(classifier,x_test,y_true,y_pred):
+def evaluate_classifier(classifier,x_test,y_true,y_pred,name):
     # https://www.researchgate.net/publication/273859036_Multi-Label_Classification_An_Overview
     #print("accuracy: {}", accuracy_score(y_true,y_pred))
     #print("haming loss: {}", hamming_loss(y_true,y_pred))
     cm = multilabel_confusion_matrix(y_true,y_pred)
     mc  = list(set(metabolism_classes.values()))
     total = 0.0
+    print(name)
     for i in range(len(cm)):
         accuracy = (cm[i][0,0]+cm[i][1,1])/sum(cm[i].ravel())
         total+=accuracy
